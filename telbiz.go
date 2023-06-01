735e755ad15f0ee5935d69390e3f63b90365a640
Jalana Vongxay <jvongxay0308@gmail.com>
2023-06-01 18:54:13 +0700

    Add comment to Message and SMS type

--- telbiz.go
+++ telbiz.go
@@ -217,6 +217,7 @@ const (
 	Unknown   = "Unknown"
 )
 
+// Message is a message to send to a phone number.
 type Message struct {
 	// To is a phone number in the format of 20xxxxxxxx or 30xxxxxxx
 	To string
@@ -227,6 +228,7 @@ type Message struct {
 	Body  string
 }
 
+// SMS is the response for SendSMS.
 type SMS struct {
 	MessageID string `json:"messageId"`
 }
