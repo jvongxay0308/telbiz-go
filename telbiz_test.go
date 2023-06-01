b04ed04a86bbea0e1951bb2caf1f4fbe38f233ab
Jalana Vongxay <jvongxay0308@gmail.com>
2023-06-01 19:07:28 +0700

    Add test validate phone number and sms title

--- telbiz_test.go
+++ telbiz_test.go
@@ -54,3 +54,55 @@ func TestSendSMS(t *testing.T) {
 		t.Fatal("expected sms")
 	}
 }
+
+func TestValidatePhoneNumber(t *testing.T) {
+	cases := []struct {
+		phoneNumber string
+		expectedErr bool
+	}{
+		{phoneNumber: "2022446688", expectedErr: false},
+		{phoneNumber: "309988776", expectedErr: false},
+
+		{phoneNumber: "02022446680", expectedErr: true},
+		{phoneNumber: "20998877660", expectedErr: true},
+		{phoneNumber: "", expectedErr: true},
+		{phoneNumber: "xyz", expectedErr: true},
+		{phoneNumber: "0022446688", expectedErr: true},
+	}
+
+	for _, c := range cases {
+		err := validatePhoneNumber(c.phoneNumber)
+		if err != nil && !c.expectedErr {
+			t.Fatalf("expected no error, got %v", err)
+		}
+		if err == nil && c.expectedErr {
+			t.Fatal("expected error")
+		}
+	}
+
+}
+
+func TestValidateSMSTitle(t *testing.T) {
+	cases := []struct {
+		title       string
+		expectedErr bool
+	}{
+		{title: Default, expectedErr: false},
+		{title: News, expectedErr: false},
+		{title: Promotion, expectedErr: false},
+		{title: Info, expectedErr: false},
+		{title: OTP, expectedErr: false},
+		{title: Unknown, expectedErr: false},
+		{title: "xyz", expectedErr: true},
+	}
+
+	for _, c := range cases {
+		err := validateTitle(c.title)
+		if err != nil && !c.expectedErr {
+			t.Fatalf("expected no error, got %v", err)
+		}
+		if err == nil && c.expectedErr {
+			t.Fatal("expected error")
+		}
+	}
+}
